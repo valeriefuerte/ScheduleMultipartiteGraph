b3a758b3d0cebf1cb2934e9428eb1ec7a16b83f3
valeria <leradopira@mail.ru>
2020-02-20 12:40:48 +0300

    Added interface prototype

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -6,15 +6,14 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->mainToolBar->hide();
 
     Router& router = Router::getInstance();
-    ui->label->setText(router.getRepository()->helloString());
+//    ui->label->setText(router.getRepository()->helloString());
 
     // установка оформления statusBar
-    ui->statusBar->setStyleSheet("background-color: #333; color: #33bb33");
-    ui->statusBar->setFont(QFont("Consolas", 14));
-    ui->statusBar->showMessage(tr("State: ready 0123456789"));
+//    ui->statusBar->setStyleSheet("background-color: #333; color: #33bb33");
+//    ui->statusBar->setFont(QFont("Consolas", 14));
+//    ui->statusBar->showMessage(tr("State: ready 0123456789"));
 }
 
 MainWindow::~MainWindow()
